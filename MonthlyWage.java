f7e627360784556a044e4c114ade5500d33c387d
Parvsub <Praveenboda421@gmail.com>
2023-02-10 21:56:13 +0530

    Added: Monthly wage is added

---
+++ MonthlyWage.java
@@ -0,0 +1,36 @@
+package com.employeewage2;
+
+public class MonthlyWage {
+    static final int FullTime = 2;
+    static final int PartTime = 1;
+    static final int WagePerHour = 20;
+
+    void employeeMonthWage(int WorkingDayPerMonth, int EmployeeWorkingHours, int EmployeeWage, int MonthlyEmployeeWage) {
+        for (int i = 0; i < WorkingDayPerMonth; i++) {
+            double employeecheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) employeecheck) {
+                case PartTime:
+                    EmployeeWorkingHours = 4;
+                    break;
+                case FullTime:
+                    EmployeeWorkingHours = 8;
+                    break;
+                default:
+                    EmployeeWorkingHours = 0;
+            }
+
+            EmployeeWage = EmployeeWorkingHours * WagePerHour;
+            MonthlyEmployeeWage += EmployeeWage;
+            System.out.println("EmployeeWage: " + EmployeeWage);
+        }
+        System.out.println("MonthlyEmployeeWage: " + MonthlyEmployeeWage);
+    }
+
+    public static void main(String[] args) {
+        MonthlyWage employeeMonth = new MonthlyWage();
+        employeeMonth.employeeMonthWage(20, 0, 0, 0);
+    }
+}
+
+
+
