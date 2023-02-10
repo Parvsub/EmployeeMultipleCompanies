778b1d4d77820127967bb31ade9b59cd160aa8eb
Parvsub <Praveenboda421@gmail.com>
2023-02-10 21:46:33 +0530

    Added:Welcome message added

---
+++ Welcomemessage.java
@@ -0,0 +1,9 @@
+package com.employeewage2;
+
+public class Welcomemessage {
+        public static void main(String[] args) {
+            System.out.println("Welcome to EmployeeWage Computation Program");
+        }
+    }
+
+
